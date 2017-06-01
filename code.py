b207c4d388602987054854611d58f649314cce6c
pxa14760 <praveenreddyarumalla@gmail.com>
2017-06-01 13:07:48 -0400

    Updated script of copy.py

--- code.py
+++ code.py
@@ -3,9 +3,12 @@ file1 = 'names.txt'
 file2 = 'details.txt'
 
 list1 = [x.strip() for x in open(file1,'r').readlines()]
+#print(list1)
 list2 = [x.strip() for x in open(file2,'r').readlines()]
+#print(list2)
 
-for line in list2:
-	numbers = [item+1 for item in range(len(list1)) if list1[item] == line]
+for line in list1:
+	#print(line)
+	numbers = [item+1 for item in range(len(list2)) if line in list2[item]]
 	myString = ",".join(map(str,numbers))
 	print(line+':'+myString)
